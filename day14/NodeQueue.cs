211e90a5c512dad7f499b4a939f470830eb6ed64
maha <mmahaece072gmail.com>
2021-11-10 11:51:58 +0530

    Add project files.

---
+++ day14/NodeQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day14
+{
+    class NodeQueue
+    {
+
+        
+        
+            public int data;
+            public NodeQueue next;
+            public NodeQueue(int data)
+            {
+                this.data = data;
+            }
+        }
+    }
+
+
---
+++ day14/NodeStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day14
+{
+   
+    
+        class NodeStack
+        {
+            public int data;
+            public NodeStack next;
+            public NodeStack(int data)
+            {
+                this.data = data;
+            }
+        }
+    }
+
